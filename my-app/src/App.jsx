50ab1d9cc67207848c05f0bce54a5396ad702069
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2024-03-25 17:53:03 +0000

    random check

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,11 +3,7 @@ import Student from './Student.jsx'
 function App() {
     return(
       <>                            
-        <Student name="Batman" age={30} isStudent={true}/>
-        <Student name="Superman" age={25} isStudent={true}/>
-        <Student name="Superman" age={25} isStudent={true}/>
-        <Student name="Superman" age={25} isStudent={true}/>
-        <Student />
+        <h1>batman</h1>
       </>
     )
 }
