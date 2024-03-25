1152698b065fb6098f861cfed4625ee63e27ab24
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2024-03-25 11:39:24 +0000

    props done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,29 +1,13 @@
-// import Header from './Header.jsx'
-// import Footer from './Footer.jsx'
-// import Food from './Food.jsx'
-import Card from './Card.jsx'
-
+import Student from './Student.jsx'
 
 function App() {
     return(
-      <> 
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <>                            
+        <Student name="Batman" age={30} isStudent={true}/>
+        <Student name="Superman" age={25} isStudent={true}/>
+        <Student name="Superman" age={25} isStudent={true}/>
+        <Student name="Superman" age={25} isStudent={true}/>
+        <Student />
       </>
     )
 }
