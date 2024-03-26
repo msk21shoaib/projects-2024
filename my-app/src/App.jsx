08f6f9a917d3e73d705b8c6fc58278c9500256fa
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2024-03-26 10:57:29 +0000

    vid6 list done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,15 +1,25 @@
-import UserGreeting from './UserGreeting.jsx'
+import List from './List.jsx'
 
 function App() {
-    return(
-      <>                         
-        <UserGreeting isLoggedIn={false} />
-        <UserGreeting isLoggedIn={true} username="thanks"/>
-        <UserGreeting/>
-        <UserGreeting/>
-        
-      </>
-    )
+  const fruits = [{id:1, name:"Apple", calories:95},
+                        {id:2, name:"Orage", calories:45},
+                        {id:3, name:"Banana", calories:105},
+                        {id:4, name:"Coconut", calories:159},
+                        {id:5, name:"Pineapple", calories:37}];  
+
+  const vegetables = [{id:6, name:"batata", calories:95},
+                      {id:7, name:"hakuna", calories:45},
+                      {id:8, name:"matata", calories:105},
+                      {id:9, name:"Coconut", calories:159},
+                      {id:10, name:"Pineapple", calories:37}];  
+
+  return(
+    <>
+      <List items = {fruits} category="Fruits" />
+      <List items = {vegetables} category="Vegetables" />
+
+    </>
+  )
 }
 
 export default App
