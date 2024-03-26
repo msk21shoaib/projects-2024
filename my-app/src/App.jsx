06cf61a3866deb9ae37fef533e7e541064327873
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2024-03-26 18:27:06 +0000

    vid 8 left

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
-// click event = an interaction when a user clicks on a.we can respond to clicks by passing a callback to the onclick event handle
-import Button from './Button.jsx'
-import Pic from './Pic.jsx'
+// React hook = Special function that allows functional components to use React features without writing class components (React v16.8) (useState, useEffect, useContext, useReducer, useCallback, and more....)
+
+//useState() = A React hook that allows the creation of a stateful variable AND a setter function to update its value in the Virtual DOM.[name, setName]
+
+import myComponent from './myComponent.jsx'
 
 function App() {
 
   return(
     <>
-      <Pic/>
+      <myComponent/>
     </>
   )
 }
---
+++ my-app/src/myComponent.jsx
@@ -0,0 +1,6 @@
+import React, {useState} from 'react'
+
+function myComponent() {
+
+}
+export default myComponent
