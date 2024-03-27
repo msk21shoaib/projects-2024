a9fb38fa8fdf3794026f48766d97fd8bebabc636
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2024-03-27 18:33:30 +0000

    vvid 9 done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,8 @@
-// React hook = Special function that allows functional components to use React features without writing class components (React v16.8) (useState, useEffect, useContext, useReducer, useCallback, and more....)
-
-//useState() = A React hook that allows the creation of a stateful variable AND a setter function to update its value in the Virtual DOM.[name, setName]
-
-import Counter from './Counter.jsx'
+import Mycmpnt from './Mycmpnt.jsx'
 
 function App() {
 
-  return( <Counter/>  );
+  return( <Mycmpnt/>  );
 }
 
 export default App
