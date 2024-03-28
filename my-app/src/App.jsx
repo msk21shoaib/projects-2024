9bdd08e3ed58d048548d872d9f7d58f55fe64b7f
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2024-03-28 09:52:11 +0000

    vid11 colorPicker

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import ColorPicker from './ColorPicker';
+import MyComponent2 from './MyComponent2';
 
 function App() {
   return (
-    <div className="app-container">
-      <ColorPicker />
+    <div className="App">
+      <h1>React Counter App</h1>
+      <MyComponent2 />
     </div>
   );
 }
