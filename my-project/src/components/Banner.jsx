0025eb93811cce028e0b5b5ef24ba70e7aefc833
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2024-04-14 14:48:31 +0000

    continue

---
+++ my-project/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+function Banner() {
+    return(
+        <div className='bg-[#2699fb] w-full py-[100px]' >
+            
+        </div>
+    )
+}
+export default Banner
