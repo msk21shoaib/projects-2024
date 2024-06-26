6d555481bad785cbcfee86dd300b8c8a03f54950
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2024-06-26 07:20:07 +0000

    useRef hook

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,24 +1,12 @@
-// useContext() => react hook that allows you to share values between multiple levels of components (nested) without passing through each level
-
-// PROVIDER COMPONENT
-// 1. import {createContext} from 'react';
-// 2. export const MyContext=createContext();
-// 3. <MyContext.Provider value={value}>
-//        <Child />
-//    </MyContext.Provider>
-
-// CONSUMER COMPONENTS
-// 1. import React, { useContext } from 'react';
-//    import { MyContext } from './ComponentA';
-// 2. const value = useContext(MyContext);
 
 import React from 'react';
-import ComponentA  from './ComponentA.jsx';
+import MyComponent7 from './MyComponent7.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <ComponentA/>
+      <MyComponent7/>
     </div>
   );
 }
