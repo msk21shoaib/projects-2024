5073a4510d9fb711520095196f5408d02d3e1cec
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2025-01-08 19:27:33 +0530

    Delete bro-code directory

--- bro-code/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer>
-            <p>&copy; My website name</p>
-        </footer>
-    );
-}
-
-export default Footer;
--- bro-code/src/Pic.jsx
+++
@@ -1,8 +0,0 @@
-function Pic(){
-
-    const imageUrl = './src/assets/ken.jpg';
-    const handleClick = (e) => e.target.style.display = "none";
-
-    return (<img onClick={(e) => handleClick(e)} src={imageUrl} ></img>)
-}
-export default Pic
