d8d4d5b396517fe13129c3550049409bba2df911
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2025-01-26 17:27:45 +0530

    goalsetter application using MERN stack

---
+++ Goalsetter/backend/controllers/goalController.js
@@ -0,0 +1,39 @@
+//@desc get goals
+//@route GET /api/goals
+//@access private
+const getGoals = (req,res) => {
+    res.status(200).json({message : 'Get goals'})
+}
+
+//@desc set goals
+//@route POST /api/goals
+//@access private
+const setGoal = (req,res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add text field')
+    }
+    res.status(200).json({ message: 'Set Goals'})
+}
+
+//@desc update goals
+//@route PUT /api/goals/:id
+//@access private
+const updateGoal = (req,res) => {
+    res.status(200).json({ message: `Update Goal ${req.params.id}`})
+}
+
+//@desc delete goals
+//@route DELETE /api/goals/:id
+//@access private
+const deleteGoal = (req,res) => {
+    res.status(200).json({ message: `Delete goal ${req.params.id}`})
+}
+
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+}
