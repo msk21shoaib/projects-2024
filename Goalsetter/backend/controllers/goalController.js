1d83dea113c2d23a74d139730c94c91b872524db
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2025-01-28 15:53:32 +0530

    Goal controlleer setup

--- Goalsetter/backend/controllers/goalController.js
+++ Goalsetter/backend/controllers/goalController.js
@@ -1,34 +1,34 @@
+const asyncHandler = require('express-async-handler')
 //@desc get goals
 //@route GET /api/goals
 //@access private
-const getGoals = (req,res) => {
+const getGoals = asyncHandler (async (req,res) => {
     res.status(200).json({message : 'Get goals'})
-}
+})
 
 //@desc set goals
 //@route POST /api/goals
 //@access private
-const setGoal = (req,res) => {
+const setGoal = asyncHandler (async (req,res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('Please add text field')
     }
     res.status(200).json({ message: 'Set Goals'})
-}
-
+})
 //@desc update goals
 //@route PUT /api/goals/:id
 //@access private
-const updateGoal = (req,res) => {
+const updateGoal = asyncHandler (async (req,res) => {
     res.status(200).json({ message: `Update Goal ${req.params.id}`})
-}
+})
 
 //@desc delete goals
 //@route DELETE /api/goals/:id
 //@access private
-const deleteGoal = (req,res) => {
+const deleteGoal = asyncHandler (async (req,res) => {
     res.status(200).json({ message: `Delete goal ${req.params.id}`})
-}
+})
 
 
 module.exports = {
