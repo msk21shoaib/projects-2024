909362b3d7a1f7c27371a8674ab9315a84888006
Muhammad Shoaib Khan <144353922+msk21shoaib@users.noreply.github.com>
2025-01-30 19:49:42 +0530

    authorization done

--- Goalsetter/backend/routes/goalRoutes.js
+++ Goalsetter/backend/routes/goalRoutes.js
@@ -2,13 +2,12 @@ const express = require('express')
 const router = express.Router()
 const {getGoals,setGoal,updateGoal,deleteGoal} = require('../controllers/goalController')
 
-router.get('/', getGoals)
+const { protect } = require('../middleware/authMiddleware.js')
 
-router.post('/', setGoal)
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id', deleteGoal)
+router.get('/', protect, getGoals)
+router.post('/', protect, setGoal)
+router.put('/:id', protect, updateGoal)
+router.delete('/:id', protect, deleteGoal)
 
 
 module.exports = router
